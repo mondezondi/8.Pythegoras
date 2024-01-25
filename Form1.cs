8b276b3d0398597ed53c81357cb540254a2aca5c
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-25 13:24:46 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8.Pythegoras
+{
+    public partial class Pythegoras1 : Form
+    {
+        public Pythegoras1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double A, B, C;
+            if (double.TryParse(textBox1.Text, out A) && double.TryParse(textBox2.Text, out B))
+            {
+                C = Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
+                textBox3.Text = C.ToString();
+            }
+        }
+    }
+}
